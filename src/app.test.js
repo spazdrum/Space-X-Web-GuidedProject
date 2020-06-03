36b48c96fea08c7c82a324e1265ced7cad782268
spazdrum <spazdrum@gmail.com>
2020-06-02 23:28:12 -0400

    initial commit 2

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
+import {
+    render,
+    fireEvent,
+    wait,
+    queryAllByTestId,
+    waitFor
+} from '@testing-library/react';
+import {
+    fetchMissions as mockFetchMissions
+} from './api/fetchMissions';
+import {
+    missionsFixture
+} from './components/MissionList.test';
+import App from './App';
+import {
+    act
+} from 'react-dom/test-utils';
 
 jest.mock("./api/fetchMissions");
 console.log("js: App.test.js: mockFetchMissions", mockFetchMissions)
 
-test("App fetches and renders missions data", () => {
+test("App fetches and renders missions data", async () => {
     // expect(false).toBe(false);
+    mockFetchMissions.mockResolvedValueOnce(missionsFixture);
+
+    const {
+        getByText,
+        queryAllByTestId
+    } = render( < App / > );
+
+    const button = getByText(/get data/i);
+
+    await waitFor(() => {
+        fireEvent.click(button);
+    })
+
+    getByText(/we are fetching data/i);
+
+    await wait();
+
+    const missionElems = await queryAllByTestId("mission");
+
+    expect(missionElems).toHaveLength(2);
+
 })
